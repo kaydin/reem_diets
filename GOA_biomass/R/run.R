e0751a83bd64616eda39589a078b17337e87f72b
Bridget Ferriss <Bridget.Ferriss@noaa.gov>
2022-05-17 20:12:06 -0700

    other biomass calc code for RACE

---
+++ GOA_biomass/R/run.R
@@ -0,0 +1,46 @@
+packages <- c("tidyverse", "RODBC", "janitor", "lubridate")
+source("R/package_check.R")
+
+
+# If local copy of racebase tables does not exist, retrieve one -----------
+
+if (!file.exists("data/local_racebase")) {
+  x <- askYesNo(msg = "No local racebase files detected in this directory. Would you like to download local copies now?")
+  if (x) {
+    dir.create("data/local_racebase", recursive = TRUE)
+    source("R/00_download_data_from_oracle.R")
+  }
+}
+
+# Prep data ---------------------------------------------------------------
+source("R/01_cleanup_data.R")
+
+
+# Get CPUE tables ---------------------------------------------------------
+source("R/02_get_cpue.R")
+
+x <- get_cpue(
+  speciescode = 30060,
+  survey_area = "GOA"
+)
+head(x)
+
+# Get biomass estimates by stratum ----------------------------------------
+
+source("R/03_get_biomass_stratum.R")
+
+y <- get_biomass_stratum(
+  speciescode = 30060,
+  survey_area = "GOA"
+)
+head(y)
+
+# Get total biomass estimates for the whole survey area -------------------
+
+source("R/04_get_biomass_total.R")
+
+z <- get_biomass_total(
+  speciescode = 30060,
+  survey_area = "GOA"
+)
+head(z)
