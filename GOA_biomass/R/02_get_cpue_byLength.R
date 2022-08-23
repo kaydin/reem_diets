bf74b9b264d4a5aba6779c9803bbe4dc6fc4c09c
BridgetFerriss <96262778+BridgetFerriss@users.noreply.github.com>
2022-08-23 09:42:51 -0700

    Add files via upload

---
+++ GOA_biomass/R/02_get_cpue_byLength.R
@@ -0,0 +1,104 @@
+# Task 1: replicate design-based total biomass per stratum that's in the SQL table
+#
+# Data and script locations -----------------------------------------------
+# CPUE table to replicate
+# SQL Developer: AFSC--> Other Users --> AI --> Tables --> CPUE
+
+# SQL script to replicate
+# G Drive: G:/GOA/biomass_sizecomp_scripts/biomass/biomass.sql
+
+# Stratum areas to use
+# SQL Developer: AFSC--> Other Users --> GOA --> Tables --> GOA_STRATA
+
+# This function replicates the tables in RACEBASE GOA --> CPUE
+
+#convert predator list to species codes (RACE) adn save RACE species codes of selected predators that want to calculate diet by stanza/length
+speciescode_list<-LWparam %>%
+  filter(sp_Cname=="walleye pollock"|sp_Cname=="rex sole"|sp_Cname=="arrowtooth flounder"|sp_Cname=="Pacific cod"|sp_Cname=="sablefish"|sp_Cname=="Pacific ocean perch"|sp_Cname=="Pacific halibut"|sp_Cname=="flathead sole")%>% #same list as "01_Diet_dataprep_length.R"
+  select(sp_code)%>%
+  distinct(.keep_all = TRUE) 
+
+
+get_cpue_Length <- function(racebase_tables = list(
+                       cruisedat = cruisedat,
+                       haul = haul,
+                       catch = catch,
+                       lengthdat = lengthdat 
+                     ),                             #BF added length file
+                     speciescode = speciescode_list$sp_code, # 21741, #predators, # POP
+                     #survey_area = "AI") 
+                     survey_area = "GOA")   {
+
+  #convert predator list to species codes (RACE)
+  
+  # survey_area is called region in RACEBASE
+  cruisedat <- racebase_tables$cruisedat
+  
+  haul <- racebase_tables$haul
+  catch <- racebase_tables$catch
+  lengthdat <- racebase_tables$lengthdat #BF added
+
+  sp_catch <- catch %>%
+    filter(species_code %in% speciescode)
+
+  dat <- haul %>%
+    left_join(cruisedat, by = c("cruisejoin", "region")) %>%
+    filter(abundance_haul == "Y" & region == survey_area) %>%
+    left_join(sp_catch, by = "hauljoin") %>% 
+    replace_na(list( weight = 0, number_fish = 0)) %>%
+    left_join(lengthdat, by = c("hauljoin", "species_code")) %>% 
+
+    dplyr::select(
+      species_code,
+      cruisejoin.x, vessel.x, haul.x,
+      haul_type, performance, duration,
+      stratum, stationid,
+      distance_fished, weight, year,
+      weight, number_fish, length, NumSamp_SpHaul, PropLength_Num, weight_Lbin,
+      start_latitude, start_longitude,
+      gear_temperature, surface_temperature,
+      AreaSwept_km2
+    ) %>%
+    dplyr::rename(
+      Lat = start_latitude,
+      Lon = start_longitude,
+      Catch_KG = weight,
+      Vessel = vessel.x,
+      Bottom_temp = gear_temperature,
+      Surface_temp = surface_temperature
+    )
+  # %>%
+  # filter(year == survey_yr)
+  
+  x <- dat %>%
+    mutate(
+      WGTCPUE = Catch_KG / AreaSwept_km2,
+      NUMCPUE = number_fish / AreaSwept_km2,
+      survey = survey_area
+    ) %>%
+    mutate (
+      NumLBin_CPUE_km2 = PropLength_Num*NUMCPUE  # Length step: convert proportion of number fish in each length bin to numberCPUE in each length bin (#fish/km2)
+    )%>%
+    mutate (WgtLBin_CPUE_km2 = NumLBin_CPUE_km2*weight_Lbin) %>% # Length step: calc weight for all fish in Lbin using proportion of CPUE (number fish in Lbin *weight)
+    #replace_na(list(species_code = speciescode)) %>% #THIS DOESN"T WORK
+    select(
+      year, survey, Vessel, haul.x, stationid,
+      stratum, distance_fished,
+      species_code, Catch_KG, number_fish,
+      AreaSwept_km2, WGTCPUE, NUMCPUE, length, NumLBin_CPUE_km2, WgtLBin_CPUE_km2
+    ) %>%
+    arrange(year)
+
+  return(x)
+}
+
+# POP: 30060
+# walleye pollock: 21740
+# sablefish: 20510
+
+# RACEBASE equivalent table: CPUE
+x <- get_cpue_Length(survey_area = "GOA", speciescode = speciescode_list$sp_code)# 21741) #POP 30060)
+head(x)
+
+
+
---
+++ GOA_biomass/R/03_get_biomass_stratum_NoLength.R
@@ -0,0 +1,97 @@
+# get_biomass_stratum
+
+#' Calculate index of total biomass per stratum
+#'
+#' @param racebase_tables a list of the cruisedat, haul, and catch tables from RACEBASE (all regions, all years)
+#' @param speciescode five-digit numeric species code from the GAP species guides
+#' @param survey_area a character code for the survey area, either "GOA" or "AI"
+#' @param vulnerability the vulnerability of the species to the survey (defaults to 1)
+#' @param strata a dataframe with the number and areas of the strata. Defaults to the strata table from RACEBASE.
+#'
+#' @return a dataframe containing the mean weighted cpue, 
+#' @export
+#'
+#' @examples
+#' x <- get_biomass_stratum(speciescode = 30060,survey_area = "GOA")
+#' head(x)
+get_biomass_stratum_noLength <- function(racebase_tables = list(
+                                  cruisedat = cruisedat,
+                                  haul = haul,
+                                  catch = catch
+                                ),
+                                speciescode = RACE, # 30060, # POP
+                                survey_area = "GOA",
+                                vulnerability = 1,
+                                strata = switch(survey_area,
+                                  "GOA" = goa_strata,
+                                  "AI" = ai_strata
+                                )) {
+  At <- sum(strata$area)
+
+  # Get cpue table
+  x <- get_cpue_noLength(
+    racebase_tables = racebase_tables,
+    survey_area = survey_area,
+    speciescode = speciescode
+  )
+
+  # Total CPUE for species, year, stratum
+  # no RACEBASE equivalent (building block of BIOMASS_STRATUM)
+  x2 <- x %>%
+    group_by(stratum) %>% #remove year
+    dplyr::summarize(
+      haul_count = length(unique(stationid)), # number of total abundance hauls
+      mean_wgt_cpue = mean(WGTCPUE),
+      var_wgt_cpue = ifelse(haul_count <= 1, NA, var(WGTCPUE) / haul_count),
+      mean_num_cpue = mean(NUMCPUE),
+      var_num_cpue = ifelse(haul_count <= 1, NA, var(NUMCPUE) / haul_count),
+      catch_count = length(which(Catch_KG > 0)) # number of hauls with nonzero catch
+    ) %>%
+    dplyr::ungroup() %>%
+    select(
+      stratum,
+      haul_count, catch_count,
+      mean_wgt_cpue, var_wgt_cpue,
+      mean_num_cpue, var_num_cpue
+    ) #remove year
+
+  if (all(x2$catch_count <= x2$haul_count)) {
+    print("Number of hauls with positive catches is realistic.")
+  }
+
+  # RACEBASE equivalent table: BIOMASS_STRATUM
+  biomass_stratum <- x2 %>%
+    dplyr::left_join(strata) %>%
+    rowwise() %>% # for applying ifelse() by row
+    mutate(
+      stratum_biomass = area * mean_wgt_cpue / vulnerability * 0.001, # kg --> mt
+      stratum_ratio = area / At,
+      biomass_var = area^2 * var_wgt_cpue * 1e-6, # kg--> mt, square it because it's variance
+      qt_size = ifelse(haul_count <= 1, 0,
+        qt(p = 0.025, df = haul_count - 1, lower.tail = F)
+      ),
+      min_biomass = stratum_biomass - qt_size * sqrt(biomass_var),
+      max_biomass = stratum_biomass + qt_size * sqrt(biomass_var),
+      stratum_pop = area * mean_num_cpue, # not sure why this calculation would be different from the stratum_biomass
+      pop_var = area^2 * var_num_cpue,
+      min_pop = stratum_pop - qt_size * sqrt(pop_var),
+      max_pop = stratum_pop + qt_size * sqrt(pop_var)
+    ) %>%
+    mutate(
+      min_biomass = ifelse(min_biomass < 0, 0, min_biomass),
+      min_pop = ifelse(min_pop < 0, 0, min_pop)
+    ) %>% # set low CI to zero if it's negative
+    add_column(RACE=speciescode) %>%
+    select(survey,stratum, RACE, stratum_ratio, haul_count, catch_count, mean_wgt_cpue, var_wgt_cpue, mean_num_cpue, var_num_cpue, stratum_biomass, biomass_var, min_biomass, max_biomass, stratum_pop, pop_var, min_pop, max_pop, area) %>% #remove year
+    mutate(
+      Ni = area / 0.01,
+      fi = (Ni * (Ni - haul_count)) / haul_count
+    )
+  return(biomass_stratum)
+}
+
+##example to check working (POP)
+#x <- get_biomass_stratum_noLength(speciescode = 30060,survey_area = "GOA") #these are RACE species codes (not NODC)
+#head(x)
+
+
