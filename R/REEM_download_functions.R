86a54997d86098deaed583ab53d8769500454173
kaydin <kerim@u.washington.edu>
2022-09-12 10:21:55 -0700

    data download cleanup

--- R/REEM_download_functions.R
+++ R/REEM_download_functions.R
@@ -11,6 +11,8 @@ query.to.csv <- function(channel, query, file){
 
 
 ##############################
+# Loops through simple name of tables (e.g. HAUL or CATCH) and queries them 
+# specifically from RACEBASE. tables using query.to.csv function.
 racebase.download <- function(channel, tables="HAUL", path="./data/local_racebase"){
   
   # Setup folders for local files -------------------------------------------
@@ -23,28 +25,91 @@ racebase.download <- function(channel, tables="HAUL", path="./data/local_racebas
   }
 }  
 
-# GOA ---------------------------------------------------------------------
-
 
 ################################
-reem.predprey.download <- function(channel, region="GOA", path="./data/local_reem"){
+#
+#
+reem.download.bts.predprey <- function(channel, region="GOA", path="data/local_reem_data"){
+
+  if(!file.exists(path)) dir.create(path, recursive = TRUE)
+
+  query_text <- paste(
+      "SELECT",
+      "NPRED.nodc as PRED_NODC, NPRED.name as PRED_NAME,",
+      "HH.REGION, HH.VESSEL, HH.CRUISE, HH.HAUL, HH.STRATUM, HH.YEAR, HH.MONTH, HH.DAY,",
+      "HH.GEAR_TEMP, HH.GEAR_DEPTH, HH.BOTTOM_DEPTH, HH.SURFACE_TEMP, HH.START_HOUR,",
+      "HH.HAULJOIN, HH.STATIONID, HH.CRUISE_TYPE,HH.RLAT, HH.RLONG,",
+      "PP.PREDJOIN, PP.PRED_LEN,",
+      "NPREY.nodc as PREY_NODC, NPREY.name as PREY_NAME,", #"NPREY.ECOPATH_Prey,", 
+      "sum(PP.PREY_TWT) as TWT, sum(PP.PREY_CNT) as CNT", 
+      "FROM",
+      "foodlab.predprey PP, foodlab.haul HH, foodlab.nodc NPRED, foodlab.nodc NPREY",
+      "WHERE",
+      "(PP.VESSEL = HH.VESSEL AND PP.CRUISE = HH.CRUISE AND PP.HAUL = HH.HAUL)", 
+      "AND (PP.PRED_NODC = NPRED.nodc AND PP.PREY_NODC = NPREY.nodc)", 
+      "AND (HH.REGION=", paste("'", region ,"')",sep=''),
+      "AND (HH.CRUISE_TYPE='Race_Groundfish')",
+      #"AND (HH.YEAR <= 2015)",
+      #"AND (HH.MONTH >= 5 AND HH.MONTH <= 8)",
+      "GROUP BY",
+      "PP.PREDJOIN, NPRED.nodc, NPRED.name, HH.REGION,",
+      "HH.VESSEL, HH.CRUISE, HH.HAUL, HH.STRATUM, HH.YEAR, HH.MONTH, HH.DAY,",
+      "HH.GEAR_TEMP, HH.GEAR_DEPTH, HH.BOTTOM_DEPTH, HH.SURFACE_TEMP,",
+      "HH.START_HOUR, HH.HAULJOIN, HH.STATIONID, HH.CRUISE_TYPE, HH.RLAT,",
+      "HH.RLONG, PP.PRED_LEN,",
+      "NPREY.nodc, NPREY.name", #"NPREY.ECOPATH_Prey",
+      "",
+      sep=' ')
   
-  query_text <-
-    
+      query.to.csv(channel = channel,
+               query   = query_text,
+               file    = paste(path,"/", region, "predprey.csv", sep=""))
+}
+
+################################
+#
+#
+reem.download.bts.preylenths <-function(channel, region="GOA", path=("data/local_reem_data")){
+
+  if(!file.exists(path)) dir.create(path, recursive = TRUE)  
+  
+  query_text <- paste(
+    "SELECT",
+    "NPRED.nodc as PRED_NODC, NPRED.name as PRED_NAME,",
+    "HH.REGION, HH.VESSEL, HH.CRUISE, HH.HAUL, HH.STRATUM, HH.YEAR, HH.MONTH, HH.DAY,",
+    "HH.GEAR_TEMP, HH.GEAR_DEPTH, HH.BOTTOM_DEPTH, HH.SURFACE_TEMP, HH.START_HOUR,",
+    "HH.HAULJOIN, HH.STATIONID, HH.CRUISE_TYPE,HH.RLAT, HH.RLONG,",
+    "PP.PREDJOIN, PP.PRED_LEN,",
+    "NPREY.nodc as PREY_NODC, NPREY.name as PREY_NAME,", #"NPREY.ECOPATH_Prey,", 
+    "PP.PREY_SZ1 as Prey_Size_mm, COUNT(PP.PREY_NODC) AS Freq", 
+    "FROM",
+    "foodlab.preylen PP, foodlab.haul HH, nodc NPRED, nodc NPREY", 
+    "WHERE", 
+    #"(HH.REGION='BS' OR HH.REGION='AI' OR HH.REGION='GOA')", 
+    "(PP.VESSEL=HH.VESSEL AND PP.CRUISE=HH.CRUISE AND PP.HAUL=HH.HAUL)", 
+    "AND PP.PRED_NODC=NPRED.nodc AND PP.PREY_NODC = NPREY.nodc",
+    "AND (HH.REGION=", paste("'", region ,"')",sep=''),
+    "AND (HH.CRUISE_TYPE='Race_Groundfish')",
+    "GROUP BY",
+    "PP.PREDJOIN, NPRED.nodc, NPRED.name, HH.REGION,",
+    "HH.VESSEL, HH.CRUISE, HH.HAUL, HH.STRATUM, HH.YEAR, HH.MONTH, HH.DAY,",
+    "HH.GEAR_TEMP, HH.GEAR_DEPTH, HH.BOTTOM_DEPTH, HH.SURFACE_TEMP,",
+    "HH.START_HOUR, HH.HAULJOIN, HH.STATIONID, HH.CRUISE_TYPE, HH.RLAT,",
+    "HH.RLONG, PP.PRED_LEN,",
+    "NPREY.nodc, NPREY.name,", #"NPREY.ECOPATH_Prey",
+    "PP.PREY_SZ1",
+    sep=' ')
+  
+    query.to.csv(channel = channel,
+               query   = query_text,
+               file    = paste(path,"/", region, "preylengths.csv", sep=""))
   
 }
 
-
-a<-RODBC::sqlQuery(channel, "SELECT * FROM GOA.GOA_STRATA")
-write.csv(x=a, "./data/goa_strata.csv", row.names = FALSE)
-
-
-# NODC (Food Habits) ------------------------------------------------------
-a<-RODBC::sqlQuery(channel, "SELECT * FROM FOODLAB.NODC")
-write.csv(x=a, "./data/local_reem_data/nodc.csv", row.names = FALSE)
-
-
 ###############################################################################
+# Collects username and password (using secure getPass package) and returns
+# an open oracle channel.  Need to be on VPN with appropriate ODBC drivers
+# installed for it to work.
 oracle.connect <- function(sql_schema){
   
   # Define RODBC connection to ORACLE
@@ -69,11 +134,14 @@ oracle.connect <- function(sql_schema){
   return(channel)
 }
 
+
 ##################################################################
+# Wrapper to close an Oracle connection.
 oracle.disconnect <- function(channel){
   chan <- odbcClose(channel)
   return(chan)
 }
+
 
 # OLD STUFF
 # ##################DOWNLOAD TABLES##################################
--- R/REEM_fooddata_functions.R
+++ R/REEM_fooddata_functions.R
@@ -28,30 +28,4 @@ REEM.package.load <- function(PKG){
   }
 }
 
-#-----------------------------------
-# Connects to Oracle database
-REEM.oracle.connect <- function(){
-  # Define RODBC connection to ORACLE
-  get.connected <- function(schema = sql_schema, username, password) { #BF changed from AFSC to AFSCP1 - that's how it's set up on my computer
-    (echo <- FALSE)
-    if (!hasArg(username)) {username <- getPass(msg = "Enter your ORACLE Username: ")}
-    if (!hasArg(password)) {password <- getPass(msg = "Enter your ORACLE Password: ")
-    }
-    channel <- RODBC::odbcConnect(paste(schema), paste(username), paste(password), believeNRows = FALSE)
-  }
-  
-  # Execute the connection
-  suppressWarnings(channel <- get.connected())
-  while (channel == -1) {
-    cat("Unable to connect. Username or password may be incorrect. Please re-enter.\n\n")
-    suppressWarnings(channel <- get.connected())
-  }
-  if (class(channel) == "RODBC") {
-    cat("\nSuccessfully connected to Oracle!\n\n")
-  }
-  
-  return(channel)
-}
-
-
 
