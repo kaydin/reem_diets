87855d675ce7c7deb95aba857b04e84fd9957149
kaydin <kerim@u.washington.edu>
2023-01-04 06:21:39 -0800

    recent adjustments

--- R/dietcomp_steps_george2022.R
+++ R/dietcomp_steps_george2022.R
@@ -20,13 +20,18 @@ REEM.loadclean.diets(data_path = "data/local_reem_data")
 REEM.loadclean.lookups(strata_lookup_file    = "lookups/combined_BTS_strata.csv",
                        stratum_bin_column    = "strat_groups",
                        preynames_lookup_file = "lookups/Alaska_PreyLookup_MASTER.csv",
-                       prey_guild_column     = "ecopath_prey")
+                       prey_guild_column     = "troy")
 
 pred_params <- list(
-    "P.cod"        = list(nodc="8791030401", race="21720", LCLASS=c(0,10,30,60,85,999) ) ,
-    "W.pollock"    = list(nodc="8791030701", race="21740", LCLASS=c(0,10,25,40,55,999), 
-                          bioen=list(ref="poll2015", CA=0.119, CB=-0.46, C_TM=15, C_T0=10, C_Q=2.6))
-    )
+  "P.cod"        = list(nodc="8791030401", race="21720", LCLASS=c(0,10,30,60,85,999),
+                        bioen=list(ref="cod2015", CA=0.041, CB= -0.122, C_TM=21, C_T0=13.7, C_Q=2.41)),
+  "W.pollock"    = list(nodc="8791030701", race="21740", LCLASS=c(0,10,25,40,55,999), 
+                        bioen=list(ref="poll2015", CA=0.119, CB=-0.46, C_TM=15, C_T0=10, C_Q=2.6)),
+  "Arrowtooth"   = list(nodc="8857040102", race="10110", LCLASS=c(0,10,30,50,999),
+                        bioen=list(ref="atf2015", CA=0.125, CB=-0.199, C_TM=26, C_T0=20.512, C_Q=2.497)),
+  "P.halibut"    = list(nodc="8857041901", race="10120", LCLASS=c(0,10,50,70,999),
+                        bioen=list(ref="hal2018", CA=0.0625, CB=-0.1076, C_TM=18, C_T0=12.97, C_Q=3.084))
+)
 
 # Consumption scaling (temperature-dependent) currently uses Wisconsin
 # model eq 2 parameters for Cmax (per day) and temperature corrections.  
@@ -43,29 +48,31 @@ pred_params <- list(
 
 source("R/REEM_fooddata_functions.R")
 
-this.pred  <- "W.pollock"
+predlist <- c("W.pollock","P.cod","Arrowtooth","P.halibut")
+#this.pred  <- "W.pollock"
 this.model <- "EBS"
 
-# set up len-weight regression parameters and consumption weighting
-lwp <- get_lw(predator=this.pred, model=this.model, years=1982:2021, all.data=F) 
+
+combined_cons <- NULL
+for (this.pred in predlist){
+  # set up len-weight regression parameters and consumption weighting
+  lwp <- get_lw(predator=this.pred, model=this.model, years=1982:2021, all.data=F) 
   pred_params[[this.pred]]$lw_a  = lwp$lw_a
   pred_params[[this.pred]]$lw_b  = lwp$lw_b
+  strat_lencons  <- get_stratum_length_cons(predator=this.pred, model=this.model)
+  strat_dietcons <- add_diets_to_strata_length_cons(strat_lencons, predator=this.pred, model=this.model)
+  
+  combined_cons <- rbind(combined_cons,strat_dietcons)
+}
 
-strat_lencons  <- get_stratum_length_cons(predator=this.pred, model=this.model)
-strat_dietcons <- add_diets_to_strata_length_cons(strat_lencons, predator=this.pred, model=this.model, months=5:8)
-
-write.csv(strat_dietcons,"george_poll.csv",row.names=F)
+write.csv(combined_cons,paste(this.model,"_stratcons.csv",sep=""),row.names=F)
 
 
-
-
-
-#write.csv(diet_sum,"diet2new.csv",row.names=F)
-
-## alpha_wt_m
-#prey_a <- 0.000267
-## beta_wt_m
-#prey_b <- 3.097253
+#$W.pollock$lw_a
+#[1] 0.005646066
+#
+#$W.pollock$lw_b
+#[1] 3.037561
 
 prey_params <- list(
   "W.pollock"  = list(
