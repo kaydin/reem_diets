962ca132a960d89386522b9ca8549041c72be10c
kaydin <kerim@u.washington.edu>
2023-06-14 12:32:30 -0700

    commenting biomass code

--- R/dietcomp_steps_szymon2022.R
+++ R/dietcomp_steps_szymon2022.R
@@ -17,13 +17,22 @@ REEM.loadclean.diets(data_path = "data/local_reem_data")
 
 # Load lookup tables and create summary lookups - re-run this line 
 # if lookups change.
+
+# Load Lookup (file and column) for aggregating strata (ecosystem model and stratum numbers as primary key)
+# Load Lookup (file and column) for diet items (with NODC code as primary key)
 REEM.loadclean.lookups(strata_lookup_file    = "lookups/combined_BTS_strata.csv",
                        stratum_bin_column    = "strat_groups",
                        preynames_lookup_file = "lookups/Alaska_PreyLookup_MASTER.csv",
                        prey_guild_column     = "egoa")
 
+# Load lookup file for predators.  Name needs to match the name
+# (including case sensitivity) in prey guild and race lookups
+# Ecosystem Model and name as primary key
 predlist <- read.clean.csv("lookups/Alaska_Predators_GOA.csv")
 
+# Lookup file and column for mapping RACE codes to biomass
+# Primary key is RACE codes, race lookup names should match
+# names in prey and predator lookups
 race_lookup <- read.clean.csv("lookups/goa_race_lookup_apr_04_2023.csv") %>% 
   mutate(race_guild  = .data[["final_goa"]])
 
@@ -37,9 +46,15 @@ race_lookup <- read.clean.csv("lookups/goa_race_lookup_apr_04_2023.csv") %>%
 
 bio_combined <-NULL
 
+# Can loop through multiple models, saving results in a single
+# table (bio_combined)
 for (this.model in c("EGOA","WGOA")){
 
-# Biomass pools (no size structure) biomass  
+# get_cpue_all() mirrors the RACE get_cpue function (returning by haul), except it gets
+# biomasses for all groups at once, binned by the race_lookup names (race_guild column)
+# haul_stratum_summary() gets the total stations and area for each model domain.
+# This code groups the two together at the stratum level to get stratum biomass
+# in both tons and t/km^2
   stratsum <- get_cpue_all(model=this.model) %>%
     group_by(year, model, race_guild, stratum) %>%
     summarize(wgtcpue = sum(wgtcpue),
@@ -48,22 +63,25 @@ for (this.model in c("EGOA","WGOA")){
     mutate(bio_t_km2 = wgtcpue/1000/stations,
            bio_tons  = bio_t_km2 * area)
   
+# These two lines then sum stratsum to the total biomass and biomass density
+# for the entire model area
   model_area <- sum(strata_lookup$area[strata_lookup$model==this.model])
-  
   bio_totals <- stratsum %>%
     group_by(year, model, race_guild) %>%
     summarize(bio_tons = sum(bio_tons),
               bio_tkm2 = bio_tons/model_area, .groups="keep")
   
 
-# Split pools - loading juv/adu parameters 
+# Loop through the list of predators from the predlist table and
+# load parameters for each species into a list. The pred entries
+# are indexed by name and model
   preds      <- predlist %>% filter(model==this.model)
   pred_names <- unique(preds$predator)
   pred_params=list()
   for (p in pred_names){
     pdat <- as.list(preds[preds$predator==p,])
     pred_params[[p]] <- pdat
-    pred_params[[p]]$LCLASS <- sort(unique(c(0,pdat$juv_cm, pdat$adu_1, pdat$adu_2, pdat$adu_3,999)))
+    pred_params[[p]]$LCLASS <- sort(unique(c(0,pdat$juv_cm, pdat$adu_1, pdat$adu_2, pdat$adu_3,9999)))
     pred_params[[p]]$jsize  <- paste("[",pred_params[[p]]$LCLASS[1],",",pred_params[[p]]$LCLASS[2],")",sep='')
     pred_params[[p]]$lw_b   <- pdat$b_l_mm_g
     pred_params[[p]]$lw_a   <- pdat$a_l_mm_g*(10^pdat$b_l_mm_g)  
@@ -71,7 +89,15 @@ for (this.model in c("EGOA","WGOA")){
   }
 
   
-# Getting biomass-at-length and converting to split pool biomass  
+# Getting biomass-at-length and converting to split pool biomass
+# Loop through each predator.  the juv_adu_lencons splits out
+# length samples based on the length class divisions in the
+# predator lookup, and converts to % by weight using the a and b LW
+# regression.  This % by weight is applied to the total biomass
+# for the predator calculated above, to calculate biomass in
+# each length class.  It is output into bio_combined as two biomass groupings
+# (juv and adu) where adu is the sum of all the adu size classes.
+  
   juv_combined <- NULL
   for (p in pred_names){
     #p <- pred_names[1]
@@ -105,13 +131,17 @@ for (this.model in c("EGOA","WGOA")){
   
     bio_combined <- rbind(bio_combined,bio_with_juvs)      
   
-      
-}
+} # end of loop by model (e.g. WGOA, EGOA)
 
-write.csv(bio_combined,"goa_bio_combined_juvadu.csv",row.names=F)
+write.csv(bio_combined,"results/goa_bio_combined_juvadu.csv",row.names=F)
+
+
+# Biomass code stops here
 
 ###########################################################################
-# Diets
+# Diet data
+# to be commented later
+
 diet_combined <- NULL
 for (this.model in c("EGOA","WGOA")){
   
